506e10c0ddcd62ec48facf85a1872db39b63249d
jffry93 <jay.zalischi@gmail.com>
2021-02-10 16:11:53 -0500

    Updates

--- main.min.889c8bc8.js
+++ main.min.889c8bc8.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../img/jeffguitar.jpg":[["jeffguitar.f741b536.jpg","img/jeffguitar.jpg"],"img/jeffguitar.jpg"],"./../img/portback.jpg":[["portback.69e1e0b4.jpg","img/portback.jpg"],"img/portback.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../img/jeffguitar.jpg":[["jeffguitar.f741b536.jpg","img/jeffguitar.jpg"],"img/jeffguitar.jpg"],"./../img/smallJeffGuitar.jpg":[["smallJeffGuitar.c4946465.jpg","img/smallJeffGuitar.jpg"],"img/smallJeffGuitar.jpg"],"./../img/portback.jpg":[["portback.69e1e0b4.jpg","img/portback.jpg"],"img/portback.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52320" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53818" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
